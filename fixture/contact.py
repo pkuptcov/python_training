22de3b68bc1aa84a94cd047c2376a61046965010
Pavel Kuptcov <pavel.kuptcov@gmail.com>
2017-09-19 19:16:33 +0300

    Создан конфтест + scope = "session"

--- fixture/contact.py
+++ fixture/contact.py
@@ -102,5 +102,6 @@ class ContactHelper:
     def delete(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_css_selector('//*[@value="Delete"]').click()
-        self.return_to_contacts_page()
+        wd.find_element_by_xpath('//*[@value="Delete"]').click()
+        wd.switch_to_alert().accept()
+        wd.find_element_by_link_text("home").click()
