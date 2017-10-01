6cf2d38cf39e7d5cde0bc89e1fe49bc38a37e040
Pavel Kuptcov <pavel.kuptcov@gmail.com>
2017-10-01 21:12:28 +0300

    Создан конфтест + scope = "session"

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,14 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="123123123", header="123123123123", footer="123123123123"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
