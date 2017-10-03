ee4d512b940748279aa4c659a7b0a7498b20a5f8
Pavel Kuptcov <pavel.kuptcov@gmail.com>
2017-10-03 19:58:16 +0300

    Создан конфтест + scope = "session"

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,13 +4,19 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="123123123", header="123123123123", footer="123123123123"))
+    group = Group(name="123123123", header="123123123123", footer="123123123123")
+    app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_for_max) == sorted(new_groups, key=Group.id_for_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+ #   group = Group(name="", header="", footer="")
+#    app.group.create(group)
+ #   new_groups = app.group.get_group_list()
+ #   assert len(old_groups) + 1 == len(new_groups)
+ #   old_groups.append(group)
+  #  assert sorted(old_groups, key=Group.id_for_max) == sorted(new_groups, key=Group.id_for_max)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,8 +1,23 @@
-# -*- coding: utf-8 -*-
 from model.group import Group
 
 
-def test_edit_first_group(app):
+def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    app.group.edit(Group(name="11111111", header="11111111", footer="11111111"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_for_max) == sorted(new_groups, key=Group.id_for_max)
+
+
+#def test_modify_group_header(app):
+#    if app.group.count() == 0:
+#        app.group.create(Group(name="test"))
+#    old_groups = app.group.get_group_list()
+#    app.group.modify_first_group(Group(header="New header"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
