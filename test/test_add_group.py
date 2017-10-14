5c9333bf55850f1a0414bf17e1d8d0e9a40283b6
Pavel Kuptcov <pavel.kuptcov@gmail.com>
2017-10-14 16:07:04 +0300

    Создан конфтест + scope = "session"

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from model.group import Group
 
 
@@ -12,11 +11,11 @@ def test_add_group(app):
     assert sorted(old_groups, key=Group.id_for_max) == sorted(new_groups, key=Group.id_for_max)
 
 
-#def test_add_empty_group(app):
+# def test_add_empty_group(app):
 #    old_groups = app.group.get_group_list()
- #   group = Group(name="", header="", footer="")
+#    group = Group(name="", header="", footer="")
 #    app.group.create(group)
- #   new_groups = app.group.get_group_list()
- #   assert len(old_groups) + 1 == len(new_groups)
- #   old_groups.append(group)
-  #  assert sorted(old_groups, key=Group.id_for_max) == sorted(new_groups, key=Group.id_for_max)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) + 1 == len(new_groups)
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_for_max) == sorted(new_groups, key=Group.id_for_max)
